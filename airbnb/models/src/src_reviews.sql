8b95bcfb192ed52f34d45c70232b0c8019c071d1
Sunny <cr.chappell@pm.me>
2026-04-13 16:02:30 -0400

    Resolved connection issues and created my 1st 2 models without Jinja.

---
+++ airbnb/models/src/src_reviews.sql
@@ -0,0 +1,15 @@
+WITH
+    raw_reviews AS (
+        SELECT
+            *
+        FROM
+            airbnb.raw.raw_reviews
+    )
+SELECT
+    listing_id,
+    date as review_date,
+    reviewer_name,
+    comments as review_text,
+    sentiment as review_sentiment
+FROM
+    raw_reviews
